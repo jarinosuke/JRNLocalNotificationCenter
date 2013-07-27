30e34e69e505bc6ccaace4e158ec5a1ed4153b61
Naoki Ishikawa <naokiishikawa0808@gmail.com>
2013-07-27 16:10:16 +0900

    Created test target.

---
+++ JRNLocalNotificationCenterTest/JRNLocalNotificationCenterTest.h
@@ -0,0 +1,13 @@
+//
+//  JRNLocalNotificationCenterTest.h
+//  JRNLocalNotificationCenterTest
+//
+//  Created by jarinosuke on 7/27/13.
+//  Copyright (c) 2013 jarinosuke. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface JRNLocalNotificationCenterTest : SenTestCase
+
+@end
---
+++ JRNLocalNotificationCenterTest/JRNLocalNotificationCenterTest.m
@@ -0,0 +1,32 @@
+//
+//  JRNLocalNotificationCenterTest.m
+//  JRNLocalNotificationCenterTest
+//
+//  Created by jarinosuke on 7/27/13.
+//  Copyright (c) 2013 jarinosuke. All rights reserved.
+//
+
+#import "JRNLocalNotificationCenterTest.h"
+
+@implementation JRNLocalNotificationCenterTest
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in JRNLocalNotificationCenterTest");
+}
+
+@end
