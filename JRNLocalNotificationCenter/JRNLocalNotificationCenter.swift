3cfec352bf905a4676bdb4128009f9168188a79e
Naoki Ishikawa <naokiishikawa0808@gmail.com>
2014-06-29 13:32:21 +0900

    Added swift project file.

---
+++ JRNLocalNotificationCenter/JRNLocalNotificationCenter.swift
@@ -0,0 +1,9 @@
+//
+//  JRNLocalNotificationCenter.swift
+//  DemoApp
+//
+//  Created by Naoki Ishikawa on 6/29/14.
+//  Copyright (c) 2014 jarinosuke. All rights reserved.
+//
+
+import Foundation
